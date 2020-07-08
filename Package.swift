13c782f2a8bb78269b820a38ed8a59f9a8b243e8
Greg Galloway <greg@drawbridgenetworks.com>
2020-07-08 18:25:16 -0400

    More fixes for package file

--- Package.swift
+++ Package.swift
@@ -3,5 +3,35 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyTimer"
+    name: "SwiftyTimer",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_9),
+        .watchOS(.v3),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "SwiftyTimer iOS",
+            targets: ["SwiftyTimer"]),
+        .library(
+            name: "SwiftyTimer macOS",
+            targets: ["SwiftyTimer OS X"]),
+        .library(
+            name: "SwiftyTimer tvOS",
+            targets: ["SwiftyTimer tvOS"]),
+        .library(
+            name: "SwiftyTimer watchOS",
+            targets: ["SwiftyTimer watchOS"]),
+    ],
+    targets: [
+        .target(name: "SwiftyTimer",
+                exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]),
+        .target(name: "SwiftyTimer OS X",
+                exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]),
+        .target(name: "SwiftyTimer tvOS",
+                exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]),
+        .target(name: "SwiftyTimer watchOS",
+                exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]),
+    ]
 )
